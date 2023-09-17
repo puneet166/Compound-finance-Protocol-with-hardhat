866dfcf0d518c13952bb390efcfbcabae022cdf6
Puneet Singh <puneetsingh10091998@gmail.com>
2023-09-17 17:57:48 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,19 +10,13 @@ module.exports = {
       url: "http://localhost:8545", // This is the RPC URL of your local Görli fork
       // accounts: [""], // Use your private key from environment variables
     },
-    hardhat: {
-      // ... other configurations
-      forking: {
-        url: "https://fragrant-proportionate-butterfly.ethereum-goerli.discover.quiknode.pro/bffa06d434993eb918eb596c12db367e9bb49118/", // Replace with your Infura project ID
-      },
-    },
     mainnet: {
       url: "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID", // Replace with your Infura project ID
       // accounts: [process.env.MAINNET_PRIVATE_KEY], // Use your private key from environment variables
     },
     goerli: {
-      url: "https://ropsten.infura.io/v3/YOUR_INFURA_PROJECT_ID", // Replace with your Infura project ID
-      // accounts: [process.env.ROPSTEN_PRIVATE_KEY], // Use your private key from environment variables
+      url: "https://fragrant-proportionate-butterfly.ethereum-goerli.discover.quiknode.pro/bffa06d434993eb918eb596c12db367e9bb49118/", // Replace with your Infura project ID
+      accounts: [process.env.GOERLI_PRIVATE_KEY], // Use your private key from environment variables
     },
     // ... other networks
   },
